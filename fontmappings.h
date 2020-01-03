85e117ba49c6219288e0d76cf44fc42097756e7c
Luke Hsiao <lwhsiao@stanford.edu>
2020-01-03 14:56:39 -0800

    style(fontmappings): format with clang-format

--- fontmappings.h
+++ fontmappings.h
@@ -19,107 +19,108 @@
  * file with a new name, and change the the new file.                          *
  *******************************************************************************/
 
-
 #ifndef __SR_RESEARCH_FONTMAPPINGS_H__
 #define __SR_RESEARCH_FONTMAPPINGS_H__
-typedef struct 
-{
-	char * filename;
-	char * font_name;
-	char * font_face; /* not used for now */
-}FONT_MAP;
+typedef struct {
+    char *filename;
+    char *font_name;
+    char *font_face; /* not used for now */
+} FONT_MAP;
 
 #ifdef _WIN32 || _WIN64
-#define FONTMAPPINGS {\
-	{"Arial.ttf",						"Arial",									"Regular"},\
-	{"arialbd.ttf",						"Arial",									"Bold"},\
-	{"arialbi.ttf",						"Arial",									"Bold Italic"},\
-	{"ariali.ttf",						"Arial",									"Italic"},\
-	{"ariblk.ttf",						"Arial Black",								"Regular"},\
-	{"comic.ttf",						"Comic Sans MS",							"Regular"},\
-	{"comicbd.ttf",						"Comic Sans MS",							"Bold"},\
-	{"cour.ttf",						"Courier New",								"Regular"},\
-	{"courbd.ttf",						"Courier New",								"Bold"},\
-	{"courbi.ttf",						"Courier New",								"Bold Italic"},\
-	{"couri.ttf",						"Courier New",								"Italic"},\
-	{"georgia.ttf",						"Georgia",									"Regular"},\
-	{"georgiab.ttf",					"Georgia",									"Bold"},\
-	{"georgiai.ttf",					"Georgia",									"Italic"},\
-	{"georgiaz.ttf",					"Georgia",									"Bold Italic"},\
-	{"impact.ttf",						"Impact",									"Regular"},\
-	{"l_10646.ttf",						"Lucida Sans Unicode",						"Regular"},\
-	{"lucon.ttf",						"Lucida Console",							"Regular"},\
-	{"marlett.ttf",						"Marlett",									"Regular"},\
-	{"micross.ttf",						"Microsoft Sans Serif",						"Regular"},\
-	{"pala.ttf",						"Palatino Linotype",						"Regular"},\
-	{"palab.ttf",						"Palatino Linotype",						"Bold"},\
-	{"palabi.ttf",						"Palatino Linotype",						"Bold Italic"},\
-	{"palai.ttf",						"Palatino Linotype",						"Italic"},\
-	{"symbol.ttf",						"Symbol",									"Regular"},\
-	{"tahoma.ttf",						"Tahoma",									"Regular"},\
-	{"tahomabd.ttf",					"Tahoma",									"Bold"},\
-	{"times.ttf", 						"Times New Roman",							"Regular"},\
-	{"timesbd.ttf",						"Times New Roman",							"Bold"},\
-	{"timesbi.ttf",						"Times New Roman",							"Bold Italic"},\
-	{"timesi.ttf", 						"Times New Roman",							"Italic"},\
-	{"trebuc.ttf",						"Trebuchet MS",								"Regular"},\
-	{"trebucbd.ttf",					"Trebuchet MS",								"Bold"},\
-	{"trebucbi.ttf",					"Trebuchet MS",								"Bold Italic"},\
-	{"trebucit.ttf",					"Trebuchet MS",								"Italic"},\
-	{"verdana.ttf",						"Verdana",									"Regular"},\
-	{"verdanab.ttf",					"Verdana",									"Bold"},\
-	{"verdanai.ttf",					"Verdana",									"Italic"},\
-	{"verdanaz.ttf",					"Verdana",									"Bold Italic"},\
-	{"webdings.ttf",					"Webdings",									"Regular"},\
-	{"wingding.ttf",					"Wingdings",								"Regular"},\
-	{0,0,0}\
-	}
+#define FONTMAPPINGS                                                           \
+    {                                                                          \
+        {"Arial.ttf", "Arial", "Regular"}, {"arialbd.ttf", "Arial", "Bold"},   \
+            {"arialbi.ttf", "Arial", "Bold Italic"},                           \
+            {"ariali.ttf", "Arial", "Italic"},                                 \
+            {"ariblk.ttf", "Arial Black", "Regular"},                          \
+            {"comic.ttf", "Comic Sans MS", "Regular"},                         \
+            {"comicbd.ttf", "Comic Sans MS", "Bold"},                          \
+            {"cour.ttf", "Courier New", "Regular"},                            \
+            {"courbd.ttf", "Courier New", "Bold"},                             \
+            {"courbi.ttf", "Courier New", "Bold Italic"},                      \
+            {"couri.ttf", "Courier New", "Italic"},                            \
+            {"georgia.ttf", "Georgia", "Regular"},                             \
+            {"georgiab.ttf", "Georgia", "Bold"},                               \
+            {"georgiai.ttf", "Georgia", "Italic"},                             \
+            {"georgiaz.ttf", "Georgia", "Bold Italic"},                        \
+            {"impact.ttf", "Impact", "Regular"},                               \
+            {"l_10646.ttf", "Lucida Sans Unicode", "Regular"},                 \
+            {"lucon.ttf", "Lucida Console", "Regular"},                        \
+            {"marlett.ttf", "Marlett", "Regular"},                             \
+            {"micross.ttf", "Microsoft Sans Serif", "Regular"},                \
+            {"pala.ttf", "Palatino Linotype", "Regular"},                      \
+            {"palab.ttf", "Palatino Linotype", "Bold"},                        \
+            {"palabi.ttf", "Palatino Linotype", "Bold Italic"},                \
+            {"palai.ttf", "Palatino Linotype", "Italic"},                      \
+            {"symbol.ttf", "Symbol", "Regular"},                               \
+            {"tahoma.ttf", "Tahoma", "Regular"},                               \
+            {"tahomabd.ttf", "Tahoma", "Bold"},                                \
+            {"times.ttf", "Times New Roman", "Regular"},                       \
+            {"timesbd.ttf", "Times New Roman", "Bold"},                        \
+            {"timesbi.ttf", "Times New Roman", "Bold Italic"},                 \
+            {"timesi.ttf", "Times New Roman", "Italic"},                       \
+            {"trebuc.ttf", "Trebuchet MS", "Regular"},                         \
+            {"trebucbd.ttf", "Trebuchet MS", "Bold"},                          \
+            {"trebucbi.ttf", "Trebuchet MS", "Bold Italic"},                   \
+            {"trebucit.ttf", "Trebuchet MS", "Italic"},                        \
+            {"verdana.ttf", "Verdana", "Regular"},                             \
+            {"verdanab.ttf", "Verdana", "Bold"},                               \
+            {"verdanai.ttf", "Verdana", "Italic"},                             \
+            {"verdanaz.ttf", "Verdana", "Bold Italic"},                        \
+            {"webdings.ttf", "Webdings", "Regular"},                           \
+            {"wingding.ttf", "Wingdings", "Regular"}, {                        \
+            0, 0, 0                                                            \
+        }                                                                      \
+    }
 #else
-#define FONTMAPPINGS {\
-	{"Arial.ttf",						"Arial",									"Regular"},\
-	{"arialbd.ttf",						"Arial",									"Bold"},\
-	{"arialbi.ttf",						"Arial",									"Bold Italic"},\
-	{"ariali.ttf",						"Arial",									"Italic"},\
-	{"ariblk.ttf",						"Arial Black",								"Regular"},\
-	{"comic.ttf",						"Comic Sans MS",							"Regular"},\
-	{"comicbd.ttf",						"Comic Sans MS",							"Bold"},\
-	{"cour.ttf",						"Courier New",								"Regular"},\
-	{"courbd.ttf",						"Courier New",								"Bold"},\
-	{"courbi.ttf",						"Courier New",								"Bold Italic"},\
-	{"couri.ttf",						"Courier New",								"Italic"},\
-	{"georgia.ttf",						"Georgia",									"Regular"},\
-	{"georgiab.ttf",					"Georgia",									"Bold"},\
-	{"georgiai.ttf",					"Georgia",									"Italic"},\
-	{"georgiaz.ttf",					"Georgia",									"Bold Italic"},\
-	{"impact.ttf",						"Impact",									"Regular"},\
-	{"l_10646.ttf",						"Lucida Sans Unicode",						"Regular"},\
-	{"lucon.ttf",						"Lucida Console",							"Regular"},\
-	{"marlett.ttf",						"Marlett",									"Regular"},\
-	{"micross.ttf",						"Microsoft Sans Serif",						"Regular"},\
-	{"pala.ttf",						"Palatino Linotype",						"Regular"},\
-	{"palab.ttf",						"Palatino Linotype",						"Bold"},\
-	{"palabi.ttf",						"Palatino Linotype",						"Bold Italic"},\
-	{"palai.ttf",						"Palatino Linotype",						"Italic"},\
-	{"symbol.ttf",						"Symbol",									"Regular"},\
-	{"tahoma.ttf",						"Tahoma",									"Regular"},\
-	{"tahomabd.ttf",					"Tahoma",									"Bold"},\
-	{"Times New Roman.ttf", 	        "Times New Roman",							"Regular"},\
-	{"Times New Roman Bold.ttf",        "Times New Roman",							"Bold"},\
-	{"Times New Roman Bold Italic.ttf", "Times New Roman",							"Bold Italic"},\
-	{"Times New Roman Italic.ttf", 	    "Times New Roman",							"Italic"},\
-	{"trebuc.ttf",						"Trebuchet MS",								"Regular"},\
-	{"trebucbd.ttf",					"Trebuchet MS",								"Bold"},\
-	{"trebucbi.ttf",					"Trebuchet MS",								"Bold Italic"},\
-	{"trebucit.ttf",					"Trebuchet MS",								"Italic"},\
-	{"verdana.ttf",						"Verdana",									"Regular"},\
-	{"verdanab.ttf",					"Verdana",									"Bold"},\
-	{"verdanai.ttf",					"Verdana",									"Italic"},\
-	{"verdanaz.ttf",					"Verdana",									"Bold Italic"},\
-	{"webdings.ttf",					"Webdings",									"Regular"},\
-	{"wingding.ttf",					"Wingdings",								"Regular"},\
-	{0,0,0}\
-	}
+#define FONTMAPPINGS                                                           \
+    {                                                                          \
+        {"Arial.ttf", "Arial", "Regular"}, {"arialbd.ttf", "Arial", "Bold"},   \
+            {"arialbi.ttf", "Arial", "Bold Italic"},                           \
+            {"ariali.ttf", "Arial", "Italic"},                                 \
+            {"ariblk.ttf", "Arial Black", "Regular"},                          \
+            {"comic.ttf", "Comic Sans MS", "Regular"},                         \
+            {"comicbd.ttf", "Comic Sans MS", "Bold"},                          \
+            {"cour.ttf", "Courier New", "Regular"},                            \
+            {"courbd.ttf", "Courier New", "Bold"},                             \
+            {"courbi.ttf", "Courier New", "Bold Italic"},                      \
+            {"couri.ttf", "Courier New", "Italic"},                            \
+            {"georgia.ttf", "Georgia", "Regular"},                             \
+            {"georgiab.ttf", "Georgia", "Bold"},                               \
+            {"georgiai.ttf", "Georgia", "Italic"},                             \
+            {"georgiaz.ttf", "Georgia", "Bold Italic"},                        \
+            {"impact.ttf", "Impact", "Regular"},                               \
+            {"l_10646.ttf", "Lucida Sans Unicode", "Regular"},                 \
+            {"lucon.ttf", "Lucida Console", "Regular"},                        \
+            {"marlett.ttf", "Marlett", "Regular"},                             \
+            {"micross.ttf", "Microsoft Sans Serif", "Regular"},                \
+            {"pala.ttf", "Palatino Linotype", "Regular"},                      \
+            {"palab.ttf", "Palatino Linotype", "Bold"},                        \
+            {"palabi.ttf", "Palatino Linotype", "Bold Italic"},                \
+            {"palai.ttf", "Palatino Linotype", "Italic"},                      \
+            {"symbol.ttf", "Symbol", "Regular"},                               \
+            {"tahoma.ttf", "Tahoma", "Regular"},                               \
+            {"tahomabd.ttf", "Tahoma", "Bold"},                                \
+            {"Times New Roman.ttf", "Times New Roman", "Regular"},             \
+            {"Times New Roman Bold.ttf", "Times New Roman", "Bold"},           \
+            {"Times New Roman Bold Italic.ttf",                                \
+             "Times New Roman",                                                \
+             "Bold Italic"},                                                   \
+            {"Times New Roman Italic.ttf", "Times New Roman", "Italic"},       \
+            {"trebuc.ttf", "Trebuchet MS", "Regular"},                         \
+            {"trebucbd.ttf", "Trebuchet MS", "Bold"},                          \
+            {"trebucbi.ttf", "Trebuchet MS", "Bold Italic"},                   \
+            {"trebucit.ttf", "Trebuchet MS", "Italic"},                        \
+            {"verdana.ttf", "Verdana", "Regular"},                             \
+            {"verdanab.ttf", "Verdana", "Bold"},                               \
+            {"verdanai.ttf", "Verdana", "Italic"},                             \
+            {"verdanaz.ttf", "Verdana", "Bold Italic"},                        \
+            {"webdings.ttf", "Webdings", "Regular"},                           \
+            {"wingding.ttf", "Wingdings", "Regular"}, {                        \
+            0, 0, 0                                                            \
+        }                                                                      \
+    }
 
 #endif
 #endif
-
